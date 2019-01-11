8e563f0ea963c6c98d4dfb7bc13b5d79d9f198ec
Lupe <lupec_se@yahoo.com>
2019-01-10 23:41:12 -0700

    Updates

---
+++ precache-manifest.c500088e26622940144bf1bca0a4403f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d389ee86433f1f0044",
+    "url": "/static/css/main.1b38a16b.chunk.css"
+  },
+  {
+    "revision": "16d389ee86433f1f0044",
+    "url": "/static/js/main.16d389ee.chunk.js"
+  },
+  {
+    "revision": "9a6fe706e9aef11f02e0",
+    "url": "/static/css/1.c7bf8c09.chunk.css"
+  },
+  {
+    "revision": "9a6fe706e9aef11f02e0",
+    "url": "/static/js/1.9a6fe706.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "29aff000400dade2899b5e6446b0481b",
+    "url": "/index.html"
+  }
+];
